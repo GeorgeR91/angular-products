a0f226572e8197c5b489c102d332414a5af13dca
George Rellos <g.rellos@hotmail.com>
2023-05-07 21:45:48 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { FindComponent } from './find/find.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    FindComponent
   ],
   imports: [
     BrowserModule
